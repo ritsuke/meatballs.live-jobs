c69d7444a8ecb6ff1b29b71af47eb957a688c97d
ritsuke <ritsuke@gmail.com>
2022-08-14 13:34:12 -0500

    More logging

--- build/index.js
+++ build/index.js
@@ -10,23 +10,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 import 'dotenv/config';
 import cron from 'cron';
 import fetch from 'node-fetch';
+const NODE_ENV = process.env.NODE_ENV;
+console.info(`ENVIRONMENT: ${NODE_ENV}`);
 // Scheduled Service Call: ingest new stories from HN (default: every minute)
 const callServiceIngestNewStoriesFromHNJob = new cron.CronJob(process.env.SOURCE_HN_CRON_TIME || '* * * * *', () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const startTime = Date.now();
         console.info(`starting scheduled service call for ingesting new stories from HN with max of ${process.env.SOURCE_HN_MAX_NEW_STORIES}`);
-        const requestUrl = `${process.env.NODE_ENV === 'development'
+        const requestUrl = `${NODE_ENV === 'development'
             ? 'http://localhost:3000'
             : 'https://www.meatballs.live'}/api/services/ingest/new-stories?dataSource=hn&max=${process.env.SOURCE_HN_MAX_NEW_STORIES}`;
-        yield fetch(requestUrl, {
+        const response = yield fetch(requestUrl, {
             method: 'POST',
             headers: {
                 Authorization: `Bearer ${process.env.INGEST_API_KEY}`
             }
-        });
-        const msToComplete = Date.now() - startTime;
+        }), totalStoriesSaved = (yield response.json())
+            .data.total, msToComplete = Date.now() - startTime;
         // TODO: save msToComplete to ingest status/performance time series
-        console.info(`completed scheduled service call for ingesting new stories from HN in ${msToComplete}ms`);
+        console.info(`completed scheduled service call for ingesting new stories from HN in ${msToComplete}ms, saving ${totalStoriesSaved} new stories`);
     }
     catch (error) {
         console.error(error);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import 'dotenv/config'
 import cron from 'cron'
 import fetch from 'node-fetch'
 
+interface IngestRequestResponse {
+  data: { total: number }
+}
+
+const NODE_ENV = process.env.NODE_ENV
+
+console.info(`ENVIRONMENT: ${NODE_ENV}`)
+
 // Scheduled Service Call: ingest new stories from HN (default: every minute)
 const callServiceIngestNewStoriesFromHNJob = new cron.CronJob(
   process.env.SOURCE_HN_CRON_TIME || '* * * * *',
@@ -15,26 +23,27 @@ const callServiceIngestNewStoriesFromHNJob = new cron.CronJob(
       )
 
       const requestUrl = `${
-        process.env.NODE_ENV === 'development'
+        NODE_ENV === 'development'
           ? 'http://localhost:3000'
           : 'https://www.meatballs.live'
       }/api/services/ingest/new-stories?dataSource=hn&max=${
         process.env.SOURCE_HN_MAX_NEW_STORIES
       }`
 
-      await fetch(requestUrl, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${process.env.INGEST_API_KEY}`
-        }
-      })
-
-      const msToComplete = Date.now() - startTime
+      const response = await fetch(requestUrl, {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${process.env.INGEST_API_KEY}`
+          }
+        }),
+        totalStoriesSaved = ((await response.json()) as IngestRequestResponse)
+          .data.total,
+        msToComplete = Date.now() - startTime
 
       // TODO: save msToComplete to ingest status/performance time series
 
       console.info(
-        `completed scheduled service call for ingesting new stories from HN in ${msToComplete}ms`
+        `completed scheduled service call for ingesting new stories from HN in ${msToComplete}ms, saving ${totalStoriesSaved} new stories`
       )
     } catch (error) {
       console.error(error)
