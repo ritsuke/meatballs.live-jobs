e17653f734c71e2d593c02d41d64c541678df123
ritsuke <ritsuke@gmail.com>
2022-08-16 09:05:11 -0500

    Pause scheduled service call via env var - Requires restart

--- build/index.js
+++ build/index.js
@@ -1,7 +1,18 @@
 import 'dotenv/config';
 import { callServiceIngestNewStoriesFromHNJob, callServiceIngestStoryActivityFromHNJob } from './jobs';
 console.info(`[INFO] ENVIRONMENT: ${process.env.NODE_ENV}`);
-callServiceIngestNewStoriesFromHNJob.start();
-console.info(`[INFO:NewStories] started call service job for ingesting new stories from source HN : ${process.env.SOURCE_HN_NEW_STORIES_CRON_TIME}`);
-callServiceIngestStoryActivityFromHNJob.start();
-console.info(`[INFO:StoryActivity] started call service job for ingesting story activity from source HN : ${process.env.SOURCE_HN_STORY_ACTIVITY_CRON_TIME}`);
+// TODO: manage without restart
+if (process.env.SOURCE_HN_NEW_STORIES_PAUSED !== 'true') {
+    callServiceIngestNewStoriesFromHNJob.start();
+    console.info(`[INFO:NewStories] started call service job for ingesting new stories from source HN : ${process.env.SOURCE_HN_NEW_STORIES_CRON_TIME}`);
+}
+if (process.env.SOURCE_HN_NEW_STORIES_PAUSED === 'true') {
+    console.info(`[WARN:NewStories] call service job for ingesting new stories from source HN is paused`);
+}
+if (process.env.SOURCE_HN_STORY_ACTIVITY_PAUSED !== 'true') {
+    callServiceIngestStoryActivityFromHNJob.start();
+    console.info(`[INFO:StoryActivity] started call service job for ingesting story activity from source HN : ${process.env.SOURCE_HN_STORY_ACTIVITY_CRON_TIME}`);
+}
+if (process.env.SOURCE_HN_STORY_ACTIVITY_PAUSED === 'true') {
+    console.info(`[WARN:StoryActivity] call service job for ingesting story activity from source HN is paused`);
+}
--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,31 @@ import {
 
 console.info(`[INFO] ENVIRONMENT: ${process.env.NODE_ENV}`)
 
-callServiceIngestNewStoriesFromHNJob.start()
+// TODO: manage without restart e.g. poll service, webhook, etc.
+if (process.env.SOURCE_HN_NEW_STORIES_PAUSED !== 'true') {
+  callServiceIngestNewStoriesFromHNJob.start()
 
-console.info(
-  `[INFO:NewStories] started call service job for ingesting new stories from source HN : ${process.env.SOURCE_HN_NEW_STORIES_CRON_TIME}`
-)
+  console.info(
+    `[INFO:NewStories] started call service job for ingesting new stories from source HN : ${process.env.SOURCE_HN_NEW_STORIES_CRON_TIME}`
+  )
+}
 
-callServiceIngestStoryActivityFromHNJob.start()
+if (process.env.SOURCE_HN_NEW_STORIES_PAUSED === 'true') {
+  console.info(
+    `[WARN:NewStories] call service job for ingesting new stories from source HN is paused`
+  )
+}
 
-console.info(
-  `[INFO:StoryActivity] started call service job for ingesting story activity from source HN : ${process.env.SOURCE_HN_STORY_ACTIVITY_CRON_TIME}`
-)
+if (process.env.SOURCE_HN_STORY_ACTIVITY_PAUSED !== 'true') {
+  callServiceIngestStoryActivityFromHNJob.start()
+
+  console.info(
+    `[INFO:StoryActivity] started call service job for ingesting story activity from source HN : ${process.env.SOURCE_HN_STORY_ACTIVITY_CRON_TIME}`
+  )
+}
+
+if (process.env.SOURCE_HN_STORY_ACTIVITY_PAUSED === 'true') {
+  console.info(
+    `[WARN:StoryActivity] call service job for ingesting story activity from source HN is paused`
+  )
+}
